bc13dd08ca3cff2071356cf7d9ff6f59cc17a8d4
Dhruv Thakkar <89878775+dhruvt21@users.noreply.github.com>
2023-06-09 17:27:20 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Your-Thoughts-Website
+
+A website for publishing articles which uses flask micro web framework for backend and mysql for database.
+
+To run the application you can either use the flask command or python’s -m switch with Flask. Before you can do that you need to tell your terminal the application to work with by exporting the FLASK_APP environment variable:
+
+On Linux -
+
+> export FLASK_APP=hello.py
+> flask run
+
+On Windows -
+
+> C:\path\to\app>set FLASK_APP=hello.py
+> flask run
+
+Running on http://127.0.0.1:5000/
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': '''The pain itself is important, it will be followed by the adipiscing elit. Its also a trigger. Children are living with disease, old age and children, and they are suffering from hunger and poverty. So that it is a lot of money. Its a good time to drink. He needs to live unless he is a football player. Its a mass pillow. Let Maecenas be a great player in the league, so that the mauris can be followed. But even the members of the land. If it is not pain, it is easy for the author of the eu, but what is the protein? For it is but God's vehicles, trucks. It needs to be played with a smart touch. ''',
+            'author': 'Brad traversy',
+            'create_date': '05-21-2018',
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': '''The customer is very important, the customer will be followed by the customer. It's also a trigger. Children are living with disease, old age and children, and they are suffering from hunger and poverty. So that it is a lot of money. It's a good time to drink. He needs to live unless he is a football player. It's a mass pillow. Let Maecenas be a great player in the league, so that the mauris can be followed. But even the members of the land. If it is not pain, it is easy for the author of the eu, but what is the protein? For it is but God's vehicles, trucks. It needs to be played with a smart touch.''',
+            'author': 'Brad traversy',
+            'create_date': '05-21-2018',
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': '''The customer is very important, the customer will be followed by the customer. It's also a trigger. Children are living with disease, old age and children, and they are suffering from hunger and poverty. So that it is a lot of money. It's a good time to drink. He needs to live unless he is a football player. It's a mass pillow. Let Maecenas be a great player in the league, so that the mauris can be followed. But even the members of the land. If it is not pain, it is easy for the author of the eu, but what is the protein? For it is but God's vehicles, trucks. It needs to be played with a smart touch.''',
+            'author': 'Brad traversy',
+            'create_date': '05-21-2018',
+        },
+    ]
+    return articles
